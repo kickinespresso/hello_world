4bdf1f625d20e0af97aa4aaa0b82b30928c26c4e
Chris Desch <cdesch@gmail.com>
2015-10-04 11:53:20 -0400

    Initial commit

---
+++ spec/helpers/front_page_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the FrontPageHelper. For example:
+#
+# describe FrontPageHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe FrontPageHelper, :type => :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
